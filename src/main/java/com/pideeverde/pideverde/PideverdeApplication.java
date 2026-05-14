50f94bfc60e082cf72bf55b0d40f5ce12de4e14e
Abramluck Uga <abranurga12@gmail.com>
2026-05-14 00:22:53 -0600

    Corrigiendo formato de linea y propiedades

--- src/main/java/com/pideeverde/pideverde/PideverdeApplication.java
+++ src/main/java/com/pideeverde/pideverde/PideverdeApplication.java
@@ -1,26 +1,13 @@
 package com.pideeverde.pideverde;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 
-@Entity
-@Table(name = "registros")
-public class Lead {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    @NotBlank @Size(max = 100)
-    private String nombre;
+@SpringBootApplication
+public class PideverdeApplication {
 
-    @NotBlank @Email @Size(max = 100)
-    private String correo;
+	public static void main(String[] args) {
+		SpringApplication.run(PideverdeApplication.class, args);
+	}
 
-    @NotBlank @Size(max = 150)
-    private String facultad;
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getCorreo() { return correo; }
-    public void setCorreo(String correo) { this.correo = correo; }
-    public String getFacultad() { return facultad; }
-    public void setFacultad(String facultad) { this.facultad = facultad; }
 }
